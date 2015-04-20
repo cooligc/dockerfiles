c221b773a2e4cb899444ac81ef973c4cb270fc0e
Mattias Holmqvist <mattias.holmqvist@gmail.com>
2015-04-20 10:49:36 +0200

    Added expansion of JAVA_OPTS environment variable

--- dropwizard/run_dw.sh
+++ dropwizard/run_dw.sh
@@ -5,5 +5,9 @@ service="$SERVICE"
 echo "Starting Dropwizard service $service..."
 
 echo "Running with options: '$@'"
-    exec java -jar $@ "${service}.jar" server dev.yml
+if [ -z "$JAVA_OPTS" ]; then
+  exec java -jar $@ "${service}.jar" server dev.yml
+else
+  echo "Running with JAVA_OPTS: '$JAVA_OPTS'"
+  exec java "$JAVA_OPTS" -jar $@ "${service}.jar" server dev.yml
 fi
